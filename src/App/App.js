2ceff0f0c71514d0f74fb4b2cee027d64f3e7e83
Matt Logan <mlogan5212@gmail.com>
2020-09-12 09:39:05 -0500

    connected firebase app, added google auth to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import connection from '../helpers/data/connection';
+
 import './App.scss';
 
+connection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  signOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
+  signIn = (e) => {
+    e.preventDefault();
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleProvider);
+  }
+
   render() {
+    const { authed } = this.state;
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        {
+          authed ? (
+            <button className="btn btn-secondary" onClick={this.signOut}>Log Out</button>
+          ) : (
+            <button className="btn btn-secondary" onClick={this.signIn}> Log In</button>
+          )
+        }
       </div>
     );
   }
