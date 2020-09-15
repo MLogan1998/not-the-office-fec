8968028c02288c8cedf096cc9ade050864e85f54
Matt Logan <mlogan5212@gmail.com>
2020-09-15 18:49:31 -0500

    created carousel for landing page with reactstrap

--- src/components/pages/Hello/Hello.js
+++ src/components/pages/Hello/Hello.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import LpCarousel from './Carousel/Carousel';
+
 import './Hello.scss';
 
 class Hello extends React.Component {
@@ -12,6 +14,7 @@ class Hello extends React.Component {
     return (
       <div className="home-wrapper">
         <h2>There are other options. Break the cycle.</h2>
+        <LpCarousel />
       </div>
     );
   }
