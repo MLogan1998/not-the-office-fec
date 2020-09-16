ee96184fc18031fc04e1db03e560bc26c5c86c1a
Matt Logan <mlogan5212@gmail.com>
2020-09-15 23:15:42 -0500

    added random movie, and styling. wip

--- src/components/pages/RandomMovie/RandomMovie.js
+++ src/components/pages/RandomMovie/RandomMovie.js
@@ -1,11 +1,61 @@
 import React from 'react';
 
+import quoteData from '../../../helpers/data/quoteData';
+
 import './RandomMovie.scss';
 
 class RandomMovie extends React.Component {
+  state = {
+    quote: {},
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  getQuote = () => {
+    const { movie } = this.props;
+    quoteData.getQuotesByCharId(movie.quoteId)
+      .then((quotes) => {
+        const arrLength = quotes.length;
+        const index = Math.floor(Math.random() * arrLength);
+        const quote = quotes[index];
+        this.setState({ quote });
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
+    const { movie } = this.props;
+    const { quote } = this.state;
+    const movieUrl = `https://image.tmdb.org/t/p/w342${movie.poster_path}`;
+
     return (
-      <h4>Random Movie Component</h4>
+      <div className="container">
+        <div className="flip-container item-a">
+          <div className="flipper">
+            <div className="front">
+              <img className="posterimg" src={movieUrl} alt={movie.title}></img>
+            </div>
+            <div className="back backDetails">
+              <h5>{movie.title}</h5>
+              <p>{quote.quote}</p>
+            </div>
+          </div>
+        </div>
+        <div className="item-b">
+          <button className="btn btn-secondary mt-2" disabled onClick={this.watchlistClick}><i className="fas fa-eye mr-2"></i>Add to Watchlist</button>
+        </div>
+        <div className="item-d">
+          <h5>{movie.title}</h5>
+          <p><i className="fas fa-user-alt mr-2 orange"></i>{movie.character}</p>
+          <p><i className="fas fa-calendar-alt mr-2 orange"></i>{movie.release_date}</p>
+          <p><i className="fas fa-star mr-2 orange"></i>{movie.vote_average}</p>
+        </div>
+        <div className="item-f">
+          <p>{movie.overview}</p>
+        </div>
+      </div>
     );
   }
 }
--- src/helpers/data/quoteData.js
+++ src/helpers/data/quoteData.js
@@ -3,7 +3,21 @@ import apiKeys from '../apiKeys.json';
 
 const baseURL = apiKeys.firebaseConfig.databaseURL;
 
-const getQuotesByCharId = (charId) => axios.get(`${baseURL}/quotes.json?orderBy="charId"&equalTo="${charId}"`);
+const getQuotesByCharId = (charId) => new Promise((resolve, reject) => {
+  axios.get(`${baseURL}/quotes.json?orderBy="charId"&equalTo="${charId}"`)
+    .then((response) => {
+      const quoteObj = response.data;
+      const quotes = [];
+      if (quoteObj) {
+        Object.keys(quoteObj).forEach((quoteId) => {
+          quoteObj[quoteId].id = quoteId;
+          quotes.push(quoteObj[quoteId]);
+        });
+      }
+      resolve(quotes);
+    })
+    .catch((err) => reject(err));
+});
 
 export default {
   getQuotesByCharId,
